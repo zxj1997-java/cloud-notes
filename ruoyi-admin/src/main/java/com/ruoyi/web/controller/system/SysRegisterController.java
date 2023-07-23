d58e7b0395642a7be1a42ac5b6abd9e7964d21b9
zxj <62105104+zxj1997-java@users.noreply.github.com>
2023-07-23 11:15:02 +0800

    feat(注册功能): 开启注册功能

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
@@ -1,38 +1,54 @@
 package com.ruoyi.web.controller.system;
 
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.domain.entity.SysRole;
+import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.core.domain.model.RegisterBody;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.framework.web.service.SysRegisterService;
+import com.ruoyi.system.mapper.SysRoleMapper;
+import com.ruoyi.system.service.ISysConfigService;
+import com.ruoyi.system.service.ISysRoleService;
+import com.ruoyi.system.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.domain.model.RegisterBody;
-import com.ruoyi.common.utils.StringUtils;
-import com.ruoyi.framework.web.service.SysRegisterService;
-import com.ruoyi.system.service.ISysConfigService;
 
 /**
  * 注册验证
- * 
+ *
  * @author ruoyi
  */
 @RestController
-public class SysRegisterController extends BaseController
-{
+public class SysRegisterController extends BaseController {
     @Autowired
     private SysRegisterService registerService;
-
+    @Autowired
+    private ISysRoleService roleService;
     @Autowired
     private ISysConfigService configService;
+    @Autowired
+    private ISysUserService userService;
+
+    @Autowired
+    private SysRoleMapper roleMapper;
 
     @PostMapping("/register")
-    public AjaxResult register(@RequestBody RegisterBody user)
-    {
-        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
-        {
+    public AjaxResult register(@RequestBody RegisterBody user) {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser")))) {
             return error("当前系统没有开启注册功能！");
         }
+
         String msg = registerService.register(user);
+        if (StringUtils.isEmpty(msg)) {
+            SysRole role = roleMapper.checkRoleKeyUnique("common");
+            SysUser sysUser = userService.selectUserByUserName(user.getUsername());
+            Long[] userIds = new Long[]{sysUser.getUserId()};
+            roleService.insertAuthUsers(role.getRoleId(), userIds);
+        }
+
         return StringUtils.isEmpty(msg) ? success() : error(msg);
     }
 }
