331c7cb4570705e3207e62490a1d82516670236f
zxj <62105104+zxj1997-java@users.noreply.github.com>
2023-07-23 16:19:49 +0800

    feat(注册功能): 添加失去焦点时保存功能

--- UI/src/views/note/index.vue
+++ UI/src/views/note/index.vue
@@ -6,8 +6,10 @@
     <el-button v-show="showPreview==1 || showPreview==2" class="floating-button-right" color="#e9e9eb" icon="View" size="mini" @click="showPreview=showPreview==2?1:2"></el-button>
   </div>
   <Drawer :showDrawer="showDrawer" @openFile="openFile" @toggleDrawer="showDrawer = false" @toggleTheme="toggleTheme"/>
-  <MdEditor v-if="showPreview==1" v-model="noteFile.content" :preview="true" :theme="theme" :toolbars="toolbars" class="markedit" editorId="editmark" @onSave="onSave"
-            @onUploadImg="onUploadImg"/>
+  <MdEditor v-if="showPreview==1" v-model="noteFile.content" :autoDetectCode="true" :preview="true" :theme="theme" :toolbarsExclude="toolbarsExclude" class="markedit"
+            editorId="editmark" @onBlur="onBlur"
+            @onSave="onSave" @onUploadImg="onUploadImg">
+  </MdEditor>
   <MdPreview v-else-if="showPreview==2" v-model="noteFile.content" :preview="true" :theme="theme" class="markedit" editorId="editpreview"/>
   <div v-else-if="showPreview==3" style="text-align: center">
     <h1>海内存知己,天涯若比邻</h1>
@@ -27,7 +29,7 @@ const showDrawer = ref(false);
 const showPreview = ref(3);
 const theme = ref('light');
 const noteFile = ref({});
-const toolbars = ref(['bold', 'underline', 'italic', '-', 'strikeThrough', 'title', 'sub', 'sup', 'quote', 'unorderedList', 'orderedList', 'task', '-', 'codeRow', 'code', 'link', 'image', 'table', 'mermaid', 'katex', '-', 'revoke', 'next', 'save', '=', 'fullscreen', 'preview', 'catalog']);
+const toolbarsExclude = ref(['pageFullscreen', 'github', 'htmlPreview']);
 
 onMounted(() => {
   let localTheme = localStorage.getItem('cloud-note-theme');
@@ -49,6 +51,16 @@ function openFile(fileId) {
     ElNotification({
       title: response.title,
       message: '文件打开成功',
+      type: 'success',
+    })
+  })
+}
+
+function onBlur(event) {
+  noteFile.value.html = event.target.innerHTML;
+  saveNoteFile(noteFile.value).then(response => {
+    ElMessage({
+      message: '保存成功',
       type: 'success',
     })
   })
