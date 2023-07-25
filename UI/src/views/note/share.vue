94f833149cfc674e18c07767c227a5e6d49b4ff0
zxj <62105104+zxj1997-java@users.noreply.github.com>
2023-07-25 13:39:15 +0800

    refactor(全局): 分享页面加入,修改mapper的namespace

---
+++ UI/src/views/note/share.vue
@@ -0,0 +1,15 @@
+<template>
+  <MdPreview :editorId="id" :modelValue="text"/>
+  <MdCatalog :editorId="id" :scrollElement="scrollElement"/>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import {MdCatalog, MdPreview} from 'md-editor-v3';
+// preview.css相比style.css少了编辑器那部分样式
+import 'md-editor-v3/lib/preview.css';
+
+const id = 'preview-only';
+const text = ref('# Hello Editor');
+const scrollElement = document.documentElement;
+</script>
