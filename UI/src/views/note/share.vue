bbf8c0c7c80350c02dd7e3549186fae3e12058ba
zxj <62105104+zxj1997-java@users.noreply.github.com>
2023-07-26 23:33:01 +0800

    refactor(全局): 分享页面搭建完成

--- UI/src/views/note/share.vue
+++ UI/src/views/note/share.vue
@@ -1,26 +1,45 @@
 <template>
   <el-row>
-    <el-col :span="3" class="col" style="background-color: #7dae00">
-      <MdCatalog :editorId="id" :scrollElement="scrollElement"/>
+    <el-col :span="3" class="col" style="background-color: #476a8c">
+      <MdCatalog :editorId="id" :scrollElement="scrollElement" />
     </el-col>
     <el-col :span="18" class="col">
-      <MdPreview :editorId="id" :modelValue="text"/>
+      <MdPreview :editorId="id" :modelValue="text" />
     </el-col>
-    <el-col :span="3" class="col" style="background-color: #01961a">
-      <div>云笔记:程序员专属的笔记</div>
+    <el-col :span="3" class="col" style="background-color: #476a8c">
+      <h1 align="center" style="margin: 30px 0 30px; font-weight: bold;">云笔记 </h1>
+      <h6 align="center" style="margin: 30px 0 30px;">基于若依v3.8.6</h6>
+      <h4 align="center">基于SpringBoot+Vue前后端分离的Java云笔记</h4>
+      <p align="center">
+        <a href="https://gitee.com/y_project/RuoYi-Vue/stargazers"><img src="https://gitee.com/y_project/RuoYi-Vue/badge/star.svg?theme=dark"></a>
+        <a href="https://gitee.com/y_project/RuoYi-Vue"><img src="https://img.shields.io/badge/RuoYi-v3.8.6-brightgreen.svg"></a>
+        <a href="https://gitee.com/y_project/RuoYi-Vue/blob/master/LICENSE"><img src="https://img.shields.io/github/license/mashape/apistatus.svg"></a>
+      </p>
+      <p align="center">
+        平台简介
+      </p>
+      决心打造一款属于程序员的云笔记,对有道云的恶心吃相坚决抵制,免费提供给个人和企业使用
     </el-col>
   </el-row>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import {onMounted, ref} from 'vue';
 import {MdCatalog, MdPreview} from 'md-editor-v3';
-// preview.css相比style.css少了编辑器那部分样式
 import 'md-editor-v3/lib/preview.css';
+import {getNoteFile} from "@/api/note/notefile";
 
 const id = 'preview-only';
 const text = ref('# Hello Editor');
 const scrollElement = document.documentElement;
+
+onMounted(() => {
+  // 解析查询字符串，并获取 id 参数的值
+  const params = new URLSearchParams(window.location.search);
+  getNoteFile(params.get('id')).then(response => {
+    text.value = response.content;
+  })
+})
 </script>
 
 <style scoped>
